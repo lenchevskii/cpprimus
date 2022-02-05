1a4ddae6a83eec5114e65c4a824b2f5aff6309cc
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2022-02-05 16:25:38 +0300

    fire!

---
+++ cpp14-compose/composition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+ 
+template <class F, class G>
+decltype(auto) compose(F&& f, G&& g) {
+    return [=](auto x) { return f(g(x)); };
+}
+ 
+int main() {
+  std::cout << compose(sin, asin)(0.5) << "\n";
+  return 0;
+}
